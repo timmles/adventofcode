80a591d8e4a4ea1702ebfee6c97c677ee8155b30
Tim Lewis <tim.lewis@mmiholdings.co.za>
2018-12-04 13:53:57 +0200

    finished day 3

---
+++ src/main/kotlin/year2018/day3/dayThree.kt
@@ -0,0 +1,78 @@
+package year2018.day3
+
+import com.google.common.collect.ArrayTable
+import com.google.common.collect.HashBasedTable
+import kotlin.math.abs
+import kotlin.math.max
+
+class DayThree {
+
+    fun convertImportToClaims(claimsInput: List<String>): List<Claim> {
+        val regex = Regex("#(\\d*) @ (\\d*),(\\d*): (\\d*)x(\\d*)")
+        val claims: List<Claim> = claimsInput.map {
+            val matchEntire = regex.matchEntire(it)
+
+            if (matchEntire != null) {
+                val groupValues = matchEntire.groupValues
+                Claim(
+                        groupValues[1].toInt(),
+                        groupValues[2].toInt(),
+                        groupValues[3].toInt(),
+                        groupValues[4].toInt(),
+                        groupValues[5].toInt()
+                )
+            } else {
+                throw Error("Import error :: $it")
+            }
+        }
+
+        return claims
+    }
+
+    fun countOverlap(claims: List<Claim>): Int {
+        val table = createTable(claims)
+        return table!!.values().filter { it.size > 1 }.count()
+    }
+
+    fun findNoOverlap(claims: List<Claim>): Int {
+        val table = createTable(claims)
+        val values = table!!.values()
+
+        claims.forEach {claim ->
+            val count = values.filter { it.contains(claim.id) }.filter { it.size > 1 }.count()
+
+            if (count == 0) return claim.id
+        }
+
+        throw Error("Found no single elements")
+    }
+
+    private fun createTable(claims: List<Claim>): HashBasedTable<Int, Int, MutableList<Int>>? {
+        val maxX = claims.maxBy { it.x2 }!!.x2
+        val maxY = claims.maxBy { it.y2 }!!.y2
+
+        val table = HashBasedTable.create<Int, Int, MutableList<Int>>()
+
+        claims.forEach {
+            for (x in it.x1.until(it.x2)) {
+                for (y in it.y1.until(it.y2)) {
+                    if (table[x, y] == null) table.put(x, y, mutableListOf<Int>())
+                    table[x, y].add(it.id)
+                }
+            }
+        }
+
+        return table
+    }
+}
+
+class Claim(
+        val id: Int,
+        val x1: Int,
+        val y1: Int,
+        val width: Int,
+        val height: Int
+) {
+    val x2 = x1 + width
+    val y2 = y1 + height
+}
---
+++ src/test/kotlin/year2018/day3/DayThreeTest.kt
@@ -0,0 +1,57 @@
+package year2018.day3
+
+import common.Common
+import org.junit.Test
+
+class DayThreeTest {
+
+    var dayThree = DayThree()
+
+    @Test
+    fun overlapArea() {
+        val claimsInput = listOf(
+                "#1 @ 1,3: 4x4",
+                "#2 @ 3,1: 4x4",
+                "#3 @ 5,5: 2x2"
+        )
+
+        val claims = dayThree.convertImportToClaims(claimsInput)
+        val totalAreaOverlap = dayThree.countOverlap(claims)
+
+        kotlin.test.assertEquals(4, totalAreaOverlap)
+    }
+
+    @Test
+    fun overlapAreaActual() {
+        val claimsInput = Common.getFile("year2018/day3.txt").readLines()
+
+        val claims = dayThree.convertImportToClaims(claimsInput)
+        val totalAreaOverlap = dayThree.countOverlap(claims)
+
+        kotlin.test.assertEquals(101781, totalAreaOverlap)
+    }
+
+    @Test
+    fun findUnique() {
+        val claimsInput = listOf(
+                "#1 @ 1,3: 4x4",
+                "#2 @ 3,1: 4x4",
+                "#3 @ 5,5: 2x2"
+        )
+
+        val claims = dayThree.convertImportToClaims(claimsInput)
+        val totalAreaOverlap = dayThree.findNoOverlap(claims)
+
+        kotlin.test.assertEquals(3, totalAreaOverlap)
+    }
+
+    @Test
+    fun findUniqueActual() {
+        val claimsInput = Common.getFile("year2018/day3.txt").readLines()
+
+        val claims = dayThree.convertImportToClaims(claimsInput)
+        val totalAreaOverlap = dayThree.findNoOverlap(claims)
+
+        kotlin.test.assertEquals(909, totalAreaOverlap)
+    }
+}
