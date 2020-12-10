0dde7135b29b4717b2cb668da0cb1d72dbfd76fe
Tim Lewis <tim.lewis@mmiholdings.co.za>
2020-12-10 23:15:50 +0200

    comments

--- 2020/src/main/kotlin/day10/day10.kt
+++ 2020/src/main/kotlin/day10/day10.kt
@@ -33,14 +33,15 @@ class Placeholder(input: List<Int>) {
     val memTable = LongArray(adapters.size)
     memTable[adapters.lastIndex] = 1
 
-    for (index in adapters.lastIndex-1 downTo 0) {
-      val maxSize = Math.min(index + 4, adapters.lastIndex)
-      memTable[index] = (index + 1..maxSize)
-        .filter { adapters[it] <= adapters[index] + 3 }
-        .map { memTable[it] }.sum()
+    for (index in adapters.lastIndex-1 downTo 0) { // run for the back to the front
+      val maxSize = Math.min(index + 4, adapters.lastIndex) //just make sure we don't get index out of bounds at the beginning
+      memTable[index] = (index + 1..maxSize) // take the next 3 adaptors
+        .filter { adapters[it] <= adapters[index] + 3 } // filter where value is +3 or less
+        .map { memTable[it] } // map to the memoized value (i.e. the precalculated paths to the end)
+        .sum() // sum them together
     }
 
-    return memTable[0]
+    return memTable[0] // return the memoized value for the first adaptor
   }
 }
 
