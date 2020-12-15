8708ab0dd2c1bbe9bbea78691da9f8c1f92352ee
Tim Lewis <tim.lewis@mmiholdings.co.za>
2020-12-15 23:02:17 +0200

    working crm

--- 2020/src/main/kotlin/day13/day13.kt
+++ 2020/src/main/kotlin/day13/day13.kt
@@ -56,5 +56,54 @@ class Placeholder() {
     }
     return departTime
   }
+
+  private fun busIdAndOffset(input: List<String>) = input[1]
+    .split(',')
+    .mapIndexed { index, it ->
+      if (it == "x") null
+      else Pair(it.toLong(), index)
+    }.filterNotNull()
+
+  /* returns x where (a * x) % b == 1 */
+  fun multInv(a: Long, b: Long): Long {
+    if (b == 1L) return 1L
+    var aa = a
+    var bb = b
+    var x0 = 0L
+    var x1 = 1L
+    while (aa > 1) {
+      val q = aa / bb
+      var t = bb
+      bb = aa % bb
+      aa = t
+      t = x0
+      x0 = x1 - q * x0
+      x1 = t
+    }
+    if (x1 < 0) x1 += b
+    return x1
+  }
+
+  fun chineseRemainder(n: LongArray, a: LongArray): Long {
+    val prod = n.fold(1L) { acc, i -> acc * i }
+    var sum = 0L
+    for (i in 0 until n.size) {
+      val p = prod / n[i]
+      sum += a[i] * multInv(p, n[i]) * p
+    }
+    return sum % prod
+  }
+
+  fun part2_crm(input: List<String>): Long {
+    val input = input[1]
+      .split(',')
+      .mapIndexed { index, it ->
+        if (it == "x") null
+        else Pair(it.toLong(), if (index == 0) 0 else it.toLong() - index)
+      }.filterNotNull()
+
+
+    return chineseRemainder(input.map { it.first }.toLongArray(), input.map { it.second }.toLongArray())
+  }
 }
 
--- 2020/src/test/kotlin/day13/day13tests.kt
+++ 2020/src/test/kotlin/day13/day13tests.kt
@@ -52,10 +52,31 @@ internal class Day13KtTest {
     assertEquals(1202161486, Placeholder().part2_bf_modulus(listOf("", "1789,37,47,1889"), 1))
   }
 
+  @Test
+  fun example2_crm() {
+    val input = """
+      939
+      7,13,x,x,59,x,31,19
+    """.trimIndent().lines()
+
+    assertEquals(3417, Placeholder().part2_crm(listOf("", "17,x,13,19")))
+    assertEquals(754018, Placeholder().part2_crm(listOf("", "67,7,59,61")))
+    assertEquals(779210, Placeholder().part2_crm(listOf("", "67,x,7,59,61")))
+    assertEquals(1068781, Placeholder().part2_crm(input))
+    assertEquals(1261476, Placeholder().part2_crm(listOf("", "67,7,x,59,61")))
+    assertEquals(1202161486, Placeholder().part2_crm(listOf("", "1789,37,47,1889")))
+  }
+
 //  @Test 6h 46m 44s
   fun question2_bruteforce() {
     val input = Common.getFile("day13.txt").readLines()
     assertEquals(672754131923874, Placeholder().part2_bf_modulus(input, 100_000_000_000_000))
+  }
+
+  @Test
+  fun question2_crm() {
+    val input = Common.getFile("day13.txt").readLines()
+    assertEquals(672754131923874, Placeholder().part2_crm(input))
   }
 }
 
