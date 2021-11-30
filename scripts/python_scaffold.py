76bfed33b96b3338ab9e6aa98b659cef7b379e17
Tim Lewis <tim.lewis@mmiholdings.co.za>
2021-11-30 21:32:47 +0200

    completed scaffold script with first draft py template

--- scripts/python_scaffold.py
+++ scripts/python_scaffold.py
@@ -1,12 +1,26 @@
 from datetime import date
 import os
+import requests
+
+current_file_dir = os.path.dirname(__file__)
 
 # input what year to select for
 today = date.today()
 today_year = today.strftime("%Y")
 today_day = today.strftime("%d")
-year = input(f'What year would you like to play ({today_year}):') or today_year
-day = input(f'What date would you like to play ({today_day}):') or today_day
+year = int(input(f'What year would you like to play ({today_year}):') or today_year)
+day = int(input(f'What date would you like to play ({today_day}):') or today_day)
 
-challenge_path = f'{os.path.dirname(__file__)}/../{year}/{day}'
+challenge_path = f'{current_file_dir}/../{year}/{day:02d}'
 os.makedirs(challenge_path)
+
+with open(challenge_path+f'/day{day}.py', "w") as solution_file,\
+    open(f'{current_file_dir}/template/template.py') as template_file:
+    solution_file.write(template_file.read())
+
+with open(challenge_path+f'/input', "w") as input_file,\
+    open('.advent_secrets') as secrets_file:
+    key, secret = secrets_file.readline().strip().split('=')
+    link = f"https://adventofcode.com/{year}/day/{day}/input"
+    f = requests.get(link, cookies=dict(session=secret))
+    input_file.write(f.text)
---
+++ scripts/template/template.py
@@ -0,0 +1,23 @@
+# import bisect
+# import fileinput
+# import hashlib
+# import heapq
+# import itertools
+# import json
+# import re
+# from collections import defaultdict, deque, namedtuple
+
+
+def solve1(lines):
+    for line in lines:
+        print(line)
+
+
+def solve2(lines):
+    pass
+
+
+puzzle_input = open('input').read().splitlines()
+
+print('A', solve1(puzzle_input))
+print('B', solve2(puzzle_input))
