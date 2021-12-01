bc4532f81e0229cedaaf7278e5f7bcd05234ac5b
Tim Lewis <tim.lewis@mmiholdings.co.za>
2021-12-01 23:03:24 +0200

    day1 improvements

--- 2021/01/day1.py
+++ 2021/01/day1.py
@@ -9,24 +9,15 @@
 
 
 def solve1(lines):
-    measurement = None
-    count = 0
-    for line in lines:
-        if measurement is not None and measurement < int(line):
-            count = count+1
-        measurement = int(line)
-    return count
+    return [x > lines[ind - 1] if ind != 0 else False for ind, x in enumerate(lines)].count(True)
 
 
 def solve2(lines):
-    map = []
-    for i in range(2,len(lines)):
-        map.append(int(lines[i-2]) + int(lines[i-1]) + int(lines[i]))
-    return solve1(map)
+    return solve1([lines[ind-2] + lines[ind-1] + lines[ind] for ind, x in enumerate(lines[2:], 2)])
 
 
-example_input = open('example').read().splitlines()
-puzzle_input = open('input').read().splitlines()
+example_input = [int(x) for x in open('example').read().splitlines()]
+puzzle_input = [int(x) for x in open('input').read().splitlines()]
 
 print('A', solve1(example_input))
 print('A', solve1(puzzle_input))
