31fd73d073f74848145b61113d12a5cc5e3a0ea7
Tim Lewis <tim.lewis@mmiholdings.co.za>
2021-12-12 08:01:25 +0200

    day 12 first draft

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -67,7 +67,7 @@ def solve2(lines):
 example_input = open('example').read().splitlines()
 puzzle_input = open('input').read().splitlines()
 
-# print('A', solve1(example_input))
-# print('A', solve1(puzzle_input))
+print('A', solve1(example_input))
+print('A', solve1(puzzle_input))
 print('B', solve2(example_input))
 print('B', solve2(puzzle_input))
