9f15cecdc4d2d9dd55566fadb8dc1ce519369351
Tim Lewis <tim.lewis@mmiholdings.co.za>
2021-12-19 11:40:53 +0200

    day 19 start

---
+++ 2021/19/day19.py
@@ -0,0 +1,27 @@
+# import bisect
+# import fileinput
+# import hashlib
+# import heapq
+# import itertools
+# import json
+# import re
+# import heapq
+# from collections import defaultdict, deque, namedtuple, Counter
+
+
+def solve1(lines):
+    for line in lines:
+        print(line)
+
+
+def solve2(lines):
+    pass
+
+
+example_input = open('example').read().splitlines()
+puzzle_input = open('input').read().splitlines()
+
+print('A', solve1(example_input))
+print('A', solve1(puzzle_input))
+print('B', solve2(example_input))
+print('B', solve2(puzzle_input))
