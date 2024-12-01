ca7cf9f5b8bbdeff18529e13818790dee97f6f24
Tim Lewis <mail@timlew.is>
2024-12-01 19:45:38 +0200

    added ruby template support

--- scripts/aoc_scaffold.py
+++ scripts/aoc_scaffold.py
@@ -10,13 +10,21 @@ today_year = today.strftime("%Y")
 today_day = today.strftime("%d")
 year = int(input(f'What year would you like to play ({today_year}):') or today_year)
 day = int(input(f'What date would you like to play ({today_day}):') or today_day)
+lang = input(f'What language would you like to play (python|ruby):')
 
 challenge_path = f'{current_file_dir}/../{year}/{day:02d}'
 os.makedirs(challenge_path)
 
-with open(challenge_path+f'/day{day}.py', "w") as solution_file,\
-    open(f'{current_file_dir}/template/template.py') as template_file:
-    solution_file.write(template_file.read())
+if lang == "python":
+    with open(challenge_path+f'/day{day}.py', "w") as solution_file,\
+        open(f'{current_file_dir}/template/template.py') as template_file:
+        solution_file.write(template_file.read())
+elif lang == "ruby":
+    with open(challenge_path+f'/day{day}.rb', "w") as solution_file,\
+        open(f'{current_file_dir}/template/template.rb') as template_file:
+        solution_file.write(template_file.read())
+else:
+    print("unsupported language")
 
 with open(challenge_path+f'/example', "w") as solution_file:
     solution_file.writelines('')
---
+++ scripts/template/template.rb
@@ -0,0 +1,16 @@
+def solve1(lines)
+
+end
+
+def solve2(lines)
+
+end
+
+
+example_input = File.readlines('example')
+puzzle_input = File.readlines('input')
+
+puts "A #{solve1 example_input}"
+puts "A #{solve1 puzzle_input}"
+puts "B #{solve2 example_input}"
+puts "B #{solve2 puzzle_input}"
